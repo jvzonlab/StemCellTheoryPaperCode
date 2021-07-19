cc6e7dc5fd861c62e80703fde162a98c415ee8c5
Rutger Kok <rutger.kok@live.com>
2021-07-19 11:28:54 +0200

    Plot average clone size long term
    Shows growth behavior.

--- plot_two_compartment_clone_sizes_average_long_term.py
+++ plot_two_compartment_clone_sizes_average_long_term.py
@@ -1,11 +1,11 @@
-"""Different clone size distributions, short term. Comparing variability."""
+"""Different clone size distributions, long term. Comparing average clone size."""
 import numpy
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 
 from stem_cell_model import clone_size_simulator
-from stem_cell_model.clone_size_simulator import CloneSizeSimulationConfig, TimedCloneSizeSimulationConfig
-from stem_cell_model.parameters import SimulationParameters, SimulationConfig
+from stem_cell_model.clone_size_simulator import TimedCloneSizeSimulationConfig
+from stem_cell_model.parameters import SimulationParameters
 from stem_cell_model.timed_clone_size_distributions import TimedCloneSizeDistribution
 from stem_cell_model.two_compartment_model_space import run_simulation_niche
 
@@ -28,17 +28,17 @@ D = 30
 T = (16.153070175438597, 3.2357834505600382)  # Based on measured values
 
 parameters_symm_high_growth = SimulationParameters.for_D_alpha_and_phi(
-    D=D, alpha_n=0.95, alpha_m=-0.95, phi=0.95, T=T, a=10/T[0])
+    D=D, alpha_n=0.95, alpha_m=-0.95, phi=0.95, T=T, a=1/T[0])
 parameters_symm_mid_growth = SimulationParameters.for_D_alpha_and_phi(
-    D=D, alpha_n=0.5, alpha_m=-0.5, phi=0.95, T=T, a=10/T[0])
+    D=D, alpha_n=0.5, alpha_m=-0.5, phi=0.95, T=T, a=1/T[0])
 parameters_symm_low_growth = SimulationParameters.for_D_alpha_and_phi(
-    D=D, alpha_n=0.05, alpha_m=-0.05, phi=0.95, T=T, a=10/T[0])
+    D=D, alpha_n=0.05, alpha_m=-0.05, phi=0.95, T=T, a=1/T[0])
 parameters_mixed_mid_growth = SimulationParameters.for_D_alpha_and_phi(
-    D=D, alpha_n=0.5, alpha_m=-0.5, phi=0.5, T=T, a=10/T[0])
+    D=D, alpha_n=0.5, alpha_m=-0.5, phi=0.5, T=T, a=1/T[0])
 parameters_mixed_low_growth = SimulationParameters.for_D_alpha_and_phi(
-    D=D, alpha_n=0.05, alpha_m=-0.05, phi=0.5, T=T, a=10/T[0])
+    D=D, alpha_n=0.05, alpha_m=-0.05, phi=0.5, T=T, a=1/T[0])
 parameters_asymm_low_growth = SimulationParameters.for_D_alpha_and_phi(
-    D=D, alpha_n=0.05, alpha_m=-0.05, phi=0.05, T=T, a=10/T[0])
+    D=D, alpha_n=0.05, alpha_m=-0.05, phi=0.05, T=T, a=1/T[0])
 
 
 random = numpy.random.Generator(numpy.random.MT19937(seed=1))
@@ -49,33 +49,33 @@ config = TimedCloneSizeSimulationConfig(t_clone_size=t_clone_size, t_interval=t_
 fig, ((ax_bottom_left, ax_bottom_middle, ax_bottom_right), (ax_middle_left, ax_middle_middle, ax_middle_right), (ax_top_left, ax_top_middle, ax_top_right)) = plt.subplots(3, 3, sharex="all", sharey="all")
 
 # Top left panel
-results = clone_size_simulator.calculate_proliferative_in_niche_over_time(
+results = clone_size_simulator.calculate_niche_over_time(
     run_simulation_niche, config, parameters_symm_low_growth)
 _add_title(ax_top_left, "$\\alpha_n = 0.05$, $\\phi=0.95$")
 _plot_clone_sizes_over_time(ax_top_left, results, parameters_symm_low_growth)
 
 # Top middle panel
-results = clone_size_simulator.calculate_proliferative_in_niche_over_time(
+results = clone_size_simulator.calculate_niche_over_time(
     run_simulation_niche, config, parameters_symm_mid_growth)
 ax_top_middle.set_xlabel("n/<n(t)>")
 _add_title(ax_top_middle, "$\\alpha_n = 0.5$, $\\phi=0.95$")
 _plot_clone_sizes_over_time(ax_top_middle, results, parameters_symm_mid_growth)
 
 # Top right panel
-results = clone_size_simulator.calculate_proliferative_in_niche_over_time(
+results = clone_size_simulator.calculate_niche_over_time(
     run_simulation_niche, config, parameters_symm_high_growth)
 _add_title(ax_top_right, "$\\alpha_n = 0.95$, $\\phi=0.95$")
 _plot_clone_sizes_over_time(ax_top_right, results, parameters_symm_high_growth)
 
 # Middle left panel
-results = clone_size_simulator.calculate_proliferative_in_niche_over_time(
+results = clone_size_simulator.calculate_niche_over_time(
     run_simulation_niche, config, parameters_mixed_low_growth)
 _add_title(ax_middle_left, "$\\alpha_n = 0.05$, $\\phi=0.5$")
 ax_middle_left.set_ylabel("<n(t)> P_n(t)")
 _plot_clone_sizes_over_time(ax_middle_left, results, parameters_mixed_low_growth)
 
 # Middle middle panel
-results = clone_size_simulator.calculate_proliferative_in_niche_over_time(
+results = clone_size_simulator.calculate_niche_over_time(
     run_simulation_niche, config, parameters_mixed_mid_growth)
 _add_title(ax_middle_middle, "$\\alpha_n = 0.5$, $\\phi=0.5$")
 _plot_clone_sizes_over_time(ax_middle_middle, results, parameters_mixed_mid_growth)
@@ -84,7 +84,7 @@ _plot_clone_sizes_over_time(ax_middle_middle, results, parameters_mixed_mid_grow
 ax_middle_right.set_axis_off()
 
 # Bottom left panel
-results = clone_size_simulator.calculate_proliferative_in_niche_over_time(
+results = clone_size_simulator.calculate_niche_over_time(
     run_simulation_niche, config, parameters_asymm_low_growth)
 _add_title(ax_bottom_left, "$\\alpha_n = 0.05$, $\\phi=0.05$")
 _plot_clone_sizes_over_time(ax_bottom_left, results, parameters_asymm_low_growth)
