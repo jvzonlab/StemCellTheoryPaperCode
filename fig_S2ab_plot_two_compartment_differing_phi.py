7f7a71e223d12a5dc99112fed7c8f26a4e190422
Rutger Kok <rutger.kok@live.com>
2023-10-13 21:26:00 +0200

    Add extra example line

--- fig_S2ab_plot_two_compartment_differing_phi.py
+++ fig_S2ab_plot_two_compartment_differing_phi.py
@@ -125,6 +125,9 @@ def _plot_cov_of_variation_d(points: List[_SimulationForPoint]):
     point = _find_point(points, alpha_n=0.95, alpha_m=-0.7)
     ax_bottom_right.plot(point.phi_m, point.cov_of_variation_d[numpy.searchsorted(point.phi_n, 0.95)], label="alpha_n=0.95, alpha_m=-0.7, phi_n=0.95")
 
+    point = _find_point(points, alpha_n=0.95, alpha_m=-0.2)
+    ax_bottom_right.plot(point.phi_m, point.cov_of_variation_d[numpy.searchsorted(point.phi_n, 0.95)], label="alpha_n=0.95, alpha_m=-0.2, phi_n=0.95")
+
     ax_bottom_right.set_ylim(0, 0.6)
     ax_bottom_right.set_aspect("equal")
     ax_bottom_right.set_ylabel("CoV in D")
